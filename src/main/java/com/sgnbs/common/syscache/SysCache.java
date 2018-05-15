ae57e3a61a2477dce19884f2adb8cee589d1742f
tianj <tj_work@yeah.net>
2018-05-15 13:59:57 +0800

    tomcat容器启动读取直接bug

--- src/main/java/com/sgnbs/common/syscache/SysCache.java
+++ src/main/java/com/sgnbs/common/syscache/SysCache.java
@@ -10,6 +10,7 @@ import java.util.Vector;
 
 import javax.annotation.Resource;
 
+import lombok.extern.slf4j.Slf4j;
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.core.annotation.Order;
@@ -33,6 +34,7 @@ import com.sgnbs.ms.model.Fieldcheck;
  */
 @Component
 @Order(value=1)
+@Slf4j
 public class SysCache implements CommandLineRunner{
 	
 	@Resource
@@ -42,7 +44,7 @@ public class SysCache implements CommandLineRunner{
 	@SuppressWarnings("unchecked")
 	public void run(String... args) throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException, AnnoException{
 		//加载枚举项放入缓存
-		System.out.println("==========加载系统枚举项==========");
+		log.info("加载系统枚举项");
 		eumlist.addAll(fieldcheckDAO.findAll());
 		//读取个性化处理方法放到缓存
 		Field field = ClassLoader.class.getDeclaredField("classes");  
@@ -50,6 +52,9 @@ public class SysCache implements CommandLineRunner{
         Vector<Class<?>> classes = new Vector<Class<?>>();
         classes.addAll( (Vector<Class<?>>) field.get(this.getClass().getClassLoader()));
         for(Class<?> clz : classes) {
+        	if(!clz.getName().startsWith("com.sgnbs")){
+        		continue;
+			}
         	if(null!=clz.getAnnotation(Table.class)) {
         		model_map.put(clz.getSimpleName(), clz);
         	}
@@ -63,62 +68,41 @@ public class SysCache implements CommandLineRunner{
         		ToDetail toDetail = method.getAnnotation(ToDetail.class);
         		ToSave toSave = method.getAnnotation(ToSave.class);
         		DelDo delDo = method.getAnnotation(DelDo.class);
-        		if(null!=listdo) {
-        			if(null!=clz.getAnnotation(Service.class)) {
-        				if(null!=listdo_map.get(listdo.value().getSimpleName())) {
-        					throw new AnnoException();
-        				}
-        				listdo_map.put(listdo.value().getSimpleName(), new ClassMethod(clz, method));
-        			}else {
-        				throw new AnnoException();
-        			}
-        		}
-        		if(null!=saveDo) {
-        			if(null!=clz.getAnnotation(Service.class)) {
-        				if(null!=savedo_map.get(saveDo.value().getSimpleName())) {
-        					throw new AnnoException();
-        				}
-        				savedo_map.put(saveDo.value().getSimpleName(), new ClassMethod(clz, method));
-        			}else {
-        				throw new AnnoException();
-        			}
-        		}
-        		if(null!=toDetail) {
-        			if(null!=clz.getAnnotation(Service.class)) {
-        				if(null!=todetail_map.get(toDetail.value().getSimpleName())) {
-        					throw new AnnoException();
-        				}
-        				todetail_map.put(toDetail.value().getSimpleName(), new ClassMethod(clz, method));
-        			}else {
-        				throw new AnnoException();
-        			}
-        		}
-        		if(null!=delDo) {
-        			if(null!=clz.getAnnotation(Service.class)) {
-        				if(null!=todetail_map.get(delDo.value().getSimpleName())) {
-        					throw new AnnoException();
-        				}
-        				deldo_map.put(delDo.value().getSimpleName(), new ClassMethod(clz, method));
-        			}else {
-        				throw new AnnoException();
-        			}
-        		}
-        		if(null!=toSave) {
-        			if(null!=clz.getAnnotation(Service.class)) {
-        				if(null!=tosave_map.get(toSave.value().getSimpleName())) {
-        					throw new AnnoException();
-        				}
-        				tosave_map.put(toSave.value().getSimpleName(), new ClassMethod(clz, method));
-        			}else {
-        				throw new AnnoException();
-        			}
-        		}
-        	}
-        } 
+				addToMap(clz, method, null != listdo, listdo_map, listdo.value().getSimpleName());
+				addToMap(clz, method, null!=saveDo, savedo_map, saveDo.value().getSimpleName());
+				addToMap(clz, method, null!=toDetail, todetail_map, toDetail.value().getSimpleName());
+				addToMap(clz, method, null!=delDo, todetail_map, delDo.value().getSimpleName());
+				addToMap(clz, method, null!=toSave, tosave_map, toSave.value().getSimpleName());
+			}
+        }
+
+		log.info("系统枚举项加载成功");
 		
 	}
-	
-	
+
+	/**
+	 * put the table->actions relationship into cache
+	 * @param clz
+	 * @param method
+	 * @param b
+	 * @param cache_map
+	 * @param simpleName
+	 * @throws AnnoException
+	 */
+	private void addToMap(Class<?> clz, Method method, boolean b, Map<String, ClassMethod> cache_map, String simpleName) throws AnnoException {
+		if(b) {
+			if(null!=clz.getAnnotation(Service.class)) {
+				if(null!= cache_map.get(simpleName)) {
+					throw new AnnoException();
+				}
+				cache_map.put(simpleName, new ClassMethod(clz, method));
+			}else {
+				throw new AnnoException();
+			}
+		}
+	}
+
+
 	/**
 	 * 枚举项存放list
 	 */
