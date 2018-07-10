f6f36d998e2462810313454afb4ab9cdab4ce30c
tj1993 <t123465j>
2018-07-10 11:12:04 +0800

    最新初始化表结构

--- initsql/ms.sql
+++ initsql/ms.sql
@@ -42,36 +42,11 @@ CREATE TABLE `fieldcheck` (
   `fieldname` varchar(50) COLLATE utf8_bin DEFAULT NULL COMMENT '字段名称',
   `eums` varchar(1000) COLLATE utf8_bin DEFAULT NULL COMMENT '枚举项键值对。逗号隔开',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 /*Data for the table `fieldcheck` */
 
-insert  into `fieldcheck`(`id`,`tablename`,`fieldname`,`eums`) values (1,'SYS_MENU','ISSHOW','1=默认显示,2=需要权限'),(2,'SYS_ACTION','CANBATCH','1=支持,2=不支持'),(3,'SYS_ACTION','TYPE','1=跳转到页,2=弹窗,3=ajax请求,4=跳转到新页'),(4,'SYS_ACTION','ISSHOW','1=默认显示,2=需要权限'),(5,'SYS_ACTION','NEEDID','1=需要,2=不需要');
-
-/*Table structure for table `sys_action` */
-
-DROP TABLE IF EXISTS `sys_action`;
-
-CREATE TABLE `sys_action` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `url` varchar(200) COLLATE utf8_bin DEFAULT NULL COMMENT '操作请求地址',
-  `canbatch` int(11) DEFAULT NULL COMMENT '是否支持批量操作1.是。2不是',
-  `btnclass` varchar(100) COLLATE utf8_bin DEFAULT NULL COMMENT '按钮样式',
-  `name` varchar(20) COLLATE utf8_bin DEFAULT NULL COMMENT '按钮名称',
-  `menu_id` int(11) DEFAULT NULL COMMENT '所属菜单',
-  `sort` int(11) DEFAULT NULL COMMENT '排序',
-  `type` int(11) DEFAULT NULL COMMENT '1,弹窗。2跳转到页。3.ajax请求。4.跳转到空白页',
-  `isshow` int(11) DEFAULT NULL COMMENT '1.默认显示，2,需要权限',
-  `winxy` varchar(50) COLLATE utf8_bin DEFAULT NULL COMMENT '弹出框xy值',
-  `permission` varchar(30) COLLATE utf8_bin DEFAULT NULL COMMENT 'shiro权限字符串',
-  `needid` int(11) DEFAULT NULL COMMENT '是否需要id.1.需要。2不需要',
-  `menuname` varchar(50) COLLATE utf8_bin DEFAULT NULL COMMENT '所属菜单名',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
-
-/*Data for the table `sys_action` */
-
-insert  into `sys_action`(`id`,`url`,`canbatch`,`btnclass`,`name`,`menu_id`,`sort`,`type`,`isshow`,`winxy`,`permission`,`needid`,`menuname`) values (10,'/crud/tosave/sys_user',1,'xe654;','新增',10,1,2,1,'50%,50%','',2,'用户管理'),(11,'/crud/tosave/sys_user',2,'xe642;','编辑',10,2,2,1,'600px,600px','',1,'用户管理'),(12,'/crud/physicald/sys_user',1,'xe640;','删除',10,3,3,1,'','',1,'用户管理'),(13,'/user/toallotrole',2,'xe613;','分配角色',10,4,2,1,'500px,500px','user:allotrole',1,'用户管理'),(17,'/crud/tosave/sys_role',1,'xe654;','新增',11,1,2,1,'500px,500px','',2,'角色管理'),(18,'/crud/tosave/sys_role',2,'xe642;','编辑',11,2,2,1,'500px,500px','',1,'角色管理'),(19,'/crud/physicald/sys_role',1,'xe642;','删除',11,3,3,1,'','',1,'角色管理'),(20,'/role/toallotmenu',2,'xe62e;','分配菜单',11,4,2,1,'500px,500px','role:allotmenu',1,'角色管理'),(21,'/role/toallotbtn',1,'xe631;','分配操作按钮',11,5,2,1,'500px,500px','role:allotbtn',1,'角色管理');
+insert  into `fieldcheck`(`id`,`tablename`,`fieldname`,`eums`) values (1,'SYS_MENU','ISSHOW','1=默认显示,2=需要权限'),(2,'SYS_ACTION','CANBATCH','1=支持,2=不支持'),(3,'SYS_ACTION','TYPE','1=跳转到页,2=弹窗,3=ajax请求,4=跳转到新页'),(4,'SYS_ACTION','ISSHOW','1=默认显示,2=需要权限'),(5,'SYS_ACTION','NEEDID','1=需要,2=不需要'),(6,'SYS_PERMISSION','TYPE','1=默认显示,2=需要权限');
 
 /*Table structure for table `sys_log` */
 
@@ -86,11 +61,11 @@ CREATE TABLE `sys_log` (
   `type` int(11) DEFAULT NULL COMMENT '1主体操作。2.子级操作',
   `result` int(11) DEFAULT NULL COMMENT '1成功。2失败',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 /*Data for the table `sys_log` */
 
-insert  into `sys_log`(`id`,`user_id`,`actiondesc`,`actiontime`,`sonactionids`,`type`,`result`) values (1,'7e8d9c560c714897a6477bfdf51507b7','田杰 新增或编辑用户','2018-03-11 01:56:56',NULL,NULL,NULL),(2,'7e8d9c560c714897a6477bfdf51507b7','田杰 新增或编辑用户','2018-03-11 01:58:33',NULL,NULL,NULL),(3,'7e8d9c560c714897a6477bfdf51507b7','田杰 新增或修改角色','2018-03-12 12:47:39',NULL,NULL,NULL),(4,'7e8d9c560c714897a6477bfdf51507b7','田杰 角色分配菜单','2018-03-21 14:54:03',NULL,NULL,NULL);
+insert  into `sys_log`(`id`,`user_id`,`actiondesc`,`actiontime`,`sonactionids`,`type`,`result`) values (1,'7e8d9c560c714897a6477bfdf51507b7','田杰 新增或编辑用户','2018-03-11 01:56:56',NULL,NULL,NULL),(2,'7e8d9c560c714897a6477bfdf51507b7','田杰 新增或编辑用户','2018-03-11 01:58:33',NULL,NULL,NULL),(3,'7e8d9c560c714897a6477bfdf51507b7','田杰 新增或修改角色','2018-03-12 12:47:39',NULL,NULL,NULL),(4,'7e8d9c560c714897a6477bfdf51507b7','田杰 角色分配菜单','2018-03-21 14:54:03',NULL,NULL,NULL),(5,'7e8d9c560c714897a6477bfdf51507b7','田杰 角色分配按钮','2018-07-10 10:58:45',NULL,NULL,NULL);
 
 /*Table structure for table `sys_menu` */
 
@@ -112,6 +87,24 @@ CREATE TABLE `sys_menu` (
 
 insert  into `sys_menu`(`id`,`name`,`url`,`parentid`,`sort`,`icon`,`isshow`,`fixedprams`) values (1,'根菜单','',0,0,'',NULL,NULL),(9,'系统管理','',1,1,'xe614;',2,''),(10,'用户管理','/sysuser/list',9,1,'xe612;',1,''),(11,'角色管理','/sysrole/list',9,2,'xe613;',2,''),(12,'系统日志','/syslog/list',9,3,'xe63c;',2,'');
 
+/*Table structure for table `sys_permission` */
+
+DROP TABLE IF EXISTS `sys_permission`;
+
+CREATE TABLE `sys_permission` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `permission` varchar(100) COLLATE utf8_bin DEFAULT NULL COMMENT '权限字符串',
+  `remark` varchar(100) COLLATE utf8_bin DEFAULT NULL COMMENT '备注',
+  `parentid` varchar(100) COLLATE utf8_bin DEFAULT NULL COMMENT '上级id',
+  `type` int(11) DEFAULT NULL COMMENT '1父级是菜单，2父级不是',
+  `name` varchar(100) COLLATE utf8_bin DEFAULT NULL COMMENT '名称',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+/*Data for the table `sys_permission` */
+
+insert  into `sys_permission`(`id`,`permission`,`remark`,`parentid`,`type`,`name`) values (10,'user:add','','m_10',2,'新增'),(11,'user:edit','','m_10',1,'修改'),(12,'user:del','','m_10',1,'删除'),(13,'user:allotrole','','m_10',1,'分配角色'),(14,'role:add','','m_11',1,'新增'),(15,'role:edit','','m_11',1,'修改'),(16,'role:del','','m_11',1,'删除'),(17,'role:allotper','','m_11',1,'分配权限');
+
 /*Table structure for table `sys_role` */
 
 DROP TABLE IF EXISTS `sys_role`;
@@ -128,7 +121,7 @@ CREATE TABLE `sys_role` (
 
 /*Data for the table `sys_role` */
 
-insert  into `sys_role`(`id`,`name`,`menu_ids`,`createtime`,`roledesc`,`action_ids`) values ('1','超管','9,10,11,12','2017-09-19 13:54:22','这是超管','3');
+insert  into `sys_role`(`id`,`name`,`menu_ids`,`createtime`,`roledesc`,`action_ids`) values ('1','超管','9,11,12','2017-09-19 13:54:22','这是超管','10');
 
 /*Table structure for table `sys_selectky` */
 
